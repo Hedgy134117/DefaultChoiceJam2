196c764803933b182bd908a1f06c7f2a9f06cc1a
Hedgy <hedgy134117@hotmail.com>
2018-10-07 01:10:52 -0400

    Health system
    implemented a very rudementary health system.

---
+++ Assets/DisplayHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHealth : MonoBehaviour {
+
+	private float curHealth;
+    private float maxHealth;
+    public GameObject player;
+    public Text text;
+
+    // Update is called once per frame
+    void Update () {
+        curHealth = player.GetComponent<PlayerCollider>().curHealth;
+        maxHealth = player.GetComponent<PlayerCollider>().maxHealth;
+        text.text = curHealth.ToString();
+	}
+}
